2b681ddbfc87f68b8479e34c46165a5502b1bb72
vab2048 <vab2048@gmail.com>
2015-12-14 12:39:00 +0000

    Moved custom-set-faces from custom.el to relevant part of  config.org. In config.org also added powerline, undo-tree, and tried to fix colours in shell.

--- custom.el
+++ custom.el
@@ -8,7 +8,8 @@
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(haskell-mode-hook (quote (turn-on-haskell-indentation)))
  '(python-indent-offset 4)
- '(python-shell-interpreter "python3"))
+ '(python-shell-interpreter "python3")
+)
 
 
 ;; Default face
@@ -28,14 +29,3 @@
 :width 'normal 
 :foundry "unknown" 
 :family "DejaVu Sans Mono")
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(term ((t (:inherit default :foreground "white" :height 113 :width normal :family "DejaVu Sans Mono"))))
- '(term-color-blue ((t (:background "#008B8B" :foreground "#008B8B"))))
- '(term-color-red ((t (:background "tomato" :foreground "tomato")))))
-
-
