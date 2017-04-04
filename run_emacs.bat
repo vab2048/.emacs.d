77c07d3458d26ec564c6709ea5affd59b8fc739e
v <vab2048@gmail.com>
2017-04-04 15:17:03 +0100

    Add better details for making Windows emacs shortcut in run_emacs.bat

--- run_emacs.bat
+++ run_emacs.bat
@@ -8,8 +8,13 @@
 :: To pin a shortcut to this batch file to the taskbar:
 :: 
 :: 1. Create a shortcut to your batch file.
-:: 2. Get into shortcut property and change target to something like: cmd.exe /C "path-to-your-batch".
+:: 2. Get into shortcut property and change target to something like: 
+::    cmd.exe /C "path-to-your-batch"
+::    example using (optional) absolute path to cmd.exe
+::    C:\Windows\System32\cmd.exe /C "C:\Users\v\emacs-24.5-bin-i686-mingw32\run_emacs.bat"
 :: 3. Simply drag your new shortcut to the taskbar. It should now be pinnable.
+:: 4. Optionally, "right click -> properties -> change icon" to change the icon to the emacs icon.
+::    Choose an emacs binary (e.g. bin\emacs-25.1.exe) to pull the icon from the binary.
 :: See: http://superuser.com/a/193255
 :: -----------------------
 
